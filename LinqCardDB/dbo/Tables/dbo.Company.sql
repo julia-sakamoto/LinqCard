4ce366612e613d39292152a60410d98eb5433182
Jessica <jhyan0301@hotmail.com>
2018-08-01 13:17:32 -0400

    Auto stash before merge of "develop" and "origin/develop"

--- LinqCardDB/dbo/Tables/dbo.Company.sql
+++ LinqCardDB/dbo/Tables/dbo.Company.sql
@@ -3,7 +3,7 @@
     [CompanyWebsite] VARCHAR (50) NOT NULL,
     [CompanyName]    VARCHAR (50) NOT NULL,
     [CompanyType]    VARCHAR (50) NOT NULL,
-    [CompanyInfo]    VARCHAR (50) NOT NULL,
+    [CompanyInfo]    VARCHAR (MAX) NOT NULL,
     [CeoName]        VARCHAR (50) NOT NULL,
     PRIMARY KEY CLUSTERED ([CompanyId] ASC)
 );
