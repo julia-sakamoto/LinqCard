6c24cbdf489ffd7423f76941d10715cad2b56582
Unknown <snakeeaterstudio@gmail.com>
2018-08-01 12:58:25 -0400

    Deleting unneeded tables

--- LinqCardDB/dbo/Tables/Table.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Table] (
-    [DepartmentId]   INT        NOT NULL,
-    [DepartmentName] NCHAR (10) NOT NULL,
-    [EmployeeId]     INT        NOT NULL,
-    PRIMARY KEY CLUSTERED ([DepartmentId] ASC),
-    CONSTRAINT [FK_Table_ToTable] FOREIGN KEY ([EmployeeId]) REFERENCES [dbo].[Employee] ([EmployeeId])
-);
-
