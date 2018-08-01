1e398083b53c5a8a73a0202e86a682a34fd368bf
Unknown <jhyan0301@hotmail.com>
2018-08-01 12:47:22 -0400

    db
    db

---
+++ LinqCardDB/dbo/Tables/Employee.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE [dbo].[Employee] (
+    [EmployeeId]   INT          NOT NULL,
+    [Name]         VARCHAR (50) NOT NULL,
+    [JobTitle]     VARCHAR (50) NOT NULL,
+    [DepartmentId] VARCHAR (50) NOT NULL,
+    [PhoneNumber]  VARCHAR (50) NOT NULL,
+    [Hired ]       BIT          NULL,
+    [Address]      VARCHAR (50) NOT NULL,
+    [StartDate]    DATE         NOT NULL,
+    [EndDate]      DATE         NULL,
+    [CompanyId]    CHAR (6)     NOT NULL,
+    PRIMARY KEY CLUSTERED ([EmployeeId] ASC),
+    CONSTRAINT [FK_Employee_ToTable] FOREIGN KEY ([CompanyId]) REFERENCES [dbo].[Company] ([CompanyId])
+);
+
---
+++ LinqCardDB/dbo/Tables/Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table] (
+    [DepartmentId]   INT        NOT NULL,
+    [DepartmentName] NCHAR (10) NOT NULL,
+    [EmployeeId]     INT        NOT NULL,
+    PRIMARY KEY CLUSTERED ([DepartmentId] ASC),
+    CONSTRAINT [FK_Table_ToTable] FOREIGN KEY ([EmployeeId]) REFERENCES [dbo].[Employee] ([EmployeeId])
+);
+
