1263e16531f3d0765ee200bd4a54519552c75cf0
Jianlin Luo <sam-law@live.cn>
2018-08-02 13:57:57 -0400

    styling for hover effect

--- Project_linqCard/Project_linqCard/company.aspx.cs
+++ Project_linqCard/Project_linqCard/company.aspx.cs
@@ -39,44 +39,22 @@ namespace Project_linqcard
 
 
 
-            Button[] buttons = new Button[companyList.Count];
+            //Button[] buttons = new Button[companyList.Count];
             for (int i = 0; i < companyList.Count; i++)
             {
-                buttons[i] = new Button();
-                buttons[i].Text = "Select";
-                buttons[i].Visible = true;
-                buttons[i].Attributes["style"] = "border-radius:5px;background-color:darkgray;margin:auto;box-shadow: 1px 3px 8px #888888;";
-                buttons[i].CommandArgument = companyList[i].companyName;
-                buttons[i].Command += button_click;
-
-                System.Web.UI.HtmlControls.HtmlGenericControl buttonDiv =
-               new System.Web.UI.HtmlControls.HtmlGenericControl("DIV");
-                buttonDiv.ID = "button" + i;
-                buttonDiv.Attributes["style"] = "display:flex";
-
-
-
-                System.Web.UI.HtmlControls.HtmlGenericControl wrapper =
-               new System.Web.UI.HtmlControls.HtmlGenericControl("DIV");
-                wrapper.ID = "wrapper" + i;
-                wrapper.Attributes["style"] = "margin:0 10px 0 10px";
-
+               
                 System.Web.UI.HtmlControls.HtmlGenericControl cardDiv =
                 new System.Web.UI.HtmlControls.HtmlGenericControl("DIV");
                 cardDiv.ID = "card"+i;
+                cardDiv.Attributes["style"] = "margin:0 10px 0 10px";
+                cardDiv.Attributes.Add("onclick", "window.location='businessCard.aspx?companyName=" + companyList[i].companyName + "'");
 
-                cardDiv.InnerHtml = "<div class='card front'><div class='blue'></div><div class='yellow'>" +
+                cardDiv.InnerHtml = "<div class='container'><div class='card front'><div class='blue'></div><div class='yellow'>" +
                     "</div><div class='pink'></div><div class='dots'></div><div class='personal-intro'>" +
-                    "<p>"+ companyList[i].companyName +"</p></div></div>";
+                    "<p>"+ companyList[i].companyName + "</p></div></div></div>";
 
 
-                buttonDiv.Controls.Add(buttons[i]);
-                wrapper.Controls.Add(cardDiv);
-                wrapper.Controls.Add(buttonDiv);
-
-
-                
-                div1.Controls.Add(wrapper);
+                  div1.Controls.Add(cardDiv);
 
             }
 
@@ -87,7 +65,7 @@ namespace Project_linqcard
         {
             string v = e.CommandArgument.ToString();
 
-            Response.Redirect("businessCard.aspx?companyName="+v, false);
+            //Response.Redirect("businessCard.aspx?companyName="+v, false);
         }
 
     }
